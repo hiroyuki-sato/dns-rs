db3548dd9c450e80fa6f4072b73d8cf4a1ece085
Hiroyuki Sato <hiroysato@gmail.com>
2026-04-20 12:26:33 +0900

    Add tests for DNS records

--- src/dns/records.rs
+++ src/dns/records.rs
@@ -115,3 +115,163 @@ impl From<DnsClass> for u16 {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn dns_type_from_u16_known_values() {
+        assert_eq!(DnsType::from(1), DnsType::A);
+        assert_eq!(DnsType::from(2), DnsType::NS);
+        assert_eq!(DnsType::from(5), DnsType::CNAME);
+        assert_eq!(DnsType::from(6), DnsType::SOA);
+        assert_eq!(DnsType::from(12), DnsType::PTR);
+        assert_eq!(DnsType::from(15), DnsType::MX);
+        assert_eq!(DnsType::from(16), DnsType::TXT);
+        assert_eq!(DnsType::from(28), DnsType::AAAA);
+    }
+
+    #[test]
+    fn dns_type_from_u16_unknown_value() {
+        assert_eq!(DnsType::from(999), DnsType::Unknown(999));
+    }
+
+    #[test]
+    fn dns_type_into_u16_known_values() {
+        assert_eq!(u16::from(DnsType::A), 1);
+        assert_eq!(u16::from(DnsType::NS), 2);
+        assert_eq!(u16::from(DnsType::CNAME), 5);
+        assert_eq!(u16::from(DnsType::SOA), 6);
+        assert_eq!(u16::from(DnsType::PTR), 12);
+        assert_eq!(u16::from(DnsType::MX), 15);
+        assert_eq!(u16::from(DnsType::TXT), 16);
+        assert_eq!(u16::from(DnsType::AAAA), 28);
+    }
+
+    #[test]
+    fn dns_type_into_u16_unknown_value() {
+        assert_eq!(u16::from(DnsType::Unknown(999)), 999);
+    }
+
+    #[test]
+    fn dns_type_roundtrip_known_values() {
+        let values = [1u16, 2, 5, 6, 12, 15, 16, 28];
+
+        for value in values {
+            let ty = DnsType::from(value);
+            assert_eq!(u16::from(ty), value);
+        }
+    }
+
+    #[test]
+    fn dns_type_roundtrip_unknown_value() {
+        let value = 65000u16;
+        let ty = DnsType::from(value);
+        assert_eq!(ty, DnsType::Unknown(value));
+        assert_eq!(u16::from(ty), value);
+    }
+
+    #[test]
+    fn dns_class_from_u16_known_values() {
+        assert_eq!(DnsClass::from(1), DnsClass::Internet);
+        assert_eq!(DnsClass::from(3), DnsClass::Chaos);
+        assert_eq!(DnsClass::from(4), DnsClass::Hesiod);
+    }
+
+    #[test]
+    fn dns_class_from_u16_unknown_value() {
+        assert_eq!(DnsClass::from(999), DnsClass::Unknown(999));
+    }
+
+    #[test]
+    fn dns_class_into_u16_known_values() {
+        assert_eq!(u16::from(DnsClass::Internet), 1);
+        assert_eq!(u16::from(DnsClass::Chaos), 3);
+        assert_eq!(u16::from(DnsClass::Hesiod), 4);
+    }
+
+    #[test]
+    fn dns_class_into_u16_unknown_value() {
+        assert_eq!(u16::from(DnsClass::Unknown(999)), 999);
+    }
+
+    #[test]
+    fn dns_class_roundtrip_known_values() {
+        let values = [1u16, 3, 4];
+
+        for value in values {
+            let class = DnsClass::from(value);
+            assert_eq!(u16::from(class), value);
+        }
+    }
+
+    #[test]
+    fn dns_class_roundtrip_unknown_value() {
+        let value = 65000u16;
+        let class = DnsClass::from(value);
+        assert_eq!(class, DnsClass::Unknown(value));
+        assert_eq!(u16::from(class), value);
+    }
+
+    #[test]
+    fn question_can_be_constructed() {
+        let question = Question {
+            qname: "example.com".to_string(),
+            qtype: DnsType::A,
+            qclass: DnsClass::Internet,
+        };
+
+        assert_eq!(question.qname, "example.com");
+        assert_eq!(question.qtype, DnsType::A);
+        assert_eq!(question.qclass, DnsClass::Internet);
+    }
+
+    #[test]
+    fn resource_record_with_a_rdata_can_be_constructed() {
+        let rr = ResourceRecord {
+            name: "example.com".to_string(),
+            rrtype: DnsType::A,
+            class: DnsClass::Internet,
+            ttl: 300,
+            rdata: RData::A([93, 184, 216, 34]),
+        };
+
+        assert_eq!(rr.name, "example.com");
+        assert_eq!(rr.rrtype, DnsType::A);
+        assert_eq!(rr.class, DnsClass::Internet);
+        assert_eq!(rr.ttl, 300);
+        assert_eq!(rr.rdata, RData::A([93, 184, 216, 34]));
+    }
+
+    #[test]
+    fn resource_record_with_complex_rdata_can_be_constructed() {
+        let rr = ResourceRecord {
+            name: "example.com".to_string(),
+            rrtype: DnsType::MX,
+            class: DnsClass::Internet,
+            ttl: 600,
+            rdata: RData::MX {
+                preference: 10,
+                exchange: "mail.example.com".to_string(),
+            },
+        };
+
+        assert_eq!(rr.rrtype, DnsType::MX);
+        assert_eq!(
+            rr.rdata,
+            RData::MX {
+                preference: 10,
+                exchange: "mail.example.com".to_string(),
+            }
+        );
+    }
+
+    #[test]
+    fn rdata_unknown_preserves_bytes() {
+        let data = vec![0xde, 0xad, 0xbe, 0xef];
+        let rdata = RData::Unknown(data.clone());
+
+        assert_eq!(rdata, RData::Unknown(data));
+    }
+}
